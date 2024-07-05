9f9e12abf1b4223ce46502c78fd416a53cef8993
vishnu r <vishnurpillai2003@gmail.com>
2024-07-05 19:10:59 +0530

    created cart.js

---
+++ data/cart.js
@@ -0,0 +1,31 @@
+export let cart = JSON.parse(localStorage.getItem("cart"));
+
+if(!cart) {
+    cart = [];
+}
+
+function saveToStorage() {
+    localStorage.setItem("cart",JSON.stringify(cart));
+}
+
+export function addToCart(productId) {
+    let matchingItem;
+
+    if (cart) {
+        cart.forEach((cartItem) => {
+        if (cartItem.productId == productId)
+            matchingItem = cartItem;
+    })  
+    }
+    if(matchingItem) 
+        matchingItem.quantity++;
+    else {
+        cart.push({
+            productId: productId,
+            quantity: 1
+        })
+    }
+    saveToStorage();
+}
+
+

4bf21b83c34767167dcb6db22fa5df52ec5c0f91
vishnu r <vishnurpillai2003@gmail.com>
2024-07-06 19:48:57 +0530

    CartItem delete propery added

--- data/cart.js
+++ data/cart.js
@@ -28,4 +28,16 @@ export function addToCart(productId) {
     saveToStorage();
 }
 
+export function removeFromCart(productId) {
+    const newCart = [];
+
+    cart.forEach((cartItem) => {
+        if(cartItem.productId !== productId) {
+            newCart.push(cartItem);
+        }
+    })
+    cart = newCart;
+    saveToStorage();
+}
+
 

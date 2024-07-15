decc24c5e24982a6cd23202a5d91ee9cccc78709
vishnu r <vishnurpillai2003@gmail.com>
2024-07-15 23:57:51 +0530

    updated orders

--- scripts/orders.js
+++ scripts/orders.js
@@ -6,3 +6,57 @@ cart.forEach(cartItem => {
     cartQuantity += cartItem.quantity;
 });
 document.querySelector('.cart-quantity').innerHTML = cartQuantity;
+
+
+const html = ` <div class="order-container">
+          
+          <div class="order-header">
+            <div class="order-header-left-section">
+              <div class="order-date">
+                <div class="order-header-label">Order Placed:</div>
+                <div>August 12</div>
+              </div>
+              <div class="order-total">
+                <div class="order-header-label">Total:</div>
+                <div>$35.06</div>
+              </div>
+            </div>
+
+            <div class="order-header-right-section">
+              <div class="order-header-label">Order ID:</div>
+              <div>27cba69d-4c3d-4098-b42d-ac7fa62b7664</div>
+            </div>
+          </div>
+
+          <div class="order-details-grid">
+            <div class="product-image-container">
+              <img src="images/products/athletic-cotton-socks-6-pairs.jpg">
+            </div>
+
+            <div class="product-details">
+              <div class="product-name">
+                Black and Gray Athletic Cotton Socks - 6 Pairs
+              </div>
+              <div class="product-delivery-date">
+                Arriving on: August 15
+              </div>
+              <div class="product-quantity">
+                Quantity: 1
+              </div>
+              <button class="buy-again-button button-primary">
+                <img class="buy-again-icon" src="images/icons/buy-again.png">
+                <span class="buy-again-message">Buy it again</span>
+              </button>
+            </div>
+
+            <div class="product-actions">
+              <a href="tracking.html">
+                <button class="track-package-button button-secondary">
+                  Track package
+                </button>
+              </a>
+            </div>
+          </div>
+        </div>
+        `
+document.querySelector('.orders-grid').innerHTML = html;
